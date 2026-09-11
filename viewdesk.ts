ae84fd9a3be569289e625a25b43617f11683a31b
dennisnishil <dennisnishil@gmail.com>
2026-09-11 15:38:40 +0400

    Update viewdesk.ts

--- viewdesk.ts
+++ viewdesk.ts
@@ -352,7 +352,7 @@ app.get("/", (_req: Request, res: Response) => {
   <script src="/socket.io/socket.io.js"></script>
   <script>
     // Central cloud signaling server on Render.com
-    const CLOUD_SIGNALING_URL = "https://viewdesk-server-fu88x.onrender.com"; 
+    const CLOUD_SIGNALING_URL = "https://remoteview-bdjo.onrender.com"; 
 
     // Connects to Render if running as a client app, or local origin if hosted on server
     const SOCKET_URL = (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1")
